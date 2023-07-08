b4f0dbd85b4dcea4df7f192a9cf70a55544961fa
Nav-dev <65707700+Nav-dev@users.noreply.github.com>
2023-07-08 13:03:29 +0530

    created 3 pages for hr to recruit freshers

---
+++ my-app/script.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+  var header = document.querySelector(".header");
+  var hero = document.querySelector(".hero");
+
+  // Animate the header
+  header.classList.add("animated", "fadeIn");
+
+  // Animate the hero
+  hero.classList.add("animated", "fadeInUp");
+};
